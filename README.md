584f7e939c44238b8f3e3d7ef2cd2b3e19aefc45
Emily Bragg <ebragg09@gmail.com>
2016-10-14 14:08:09 -0400

    set up high level outline of project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # savage-garden
+
+Top level project for automating my carniverous plant setup. Supported plants: VFTs, sundews, and cobra lilies.
---
+++ sensors/sensor.cpp
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <stdlib>
+
+class Sensor {
+	public:
+		measure()
+		normalize()
+
+	private:
+		int range;
+		int offset;
+
+}
